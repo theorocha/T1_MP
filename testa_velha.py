1e8f662fc7bade0f8769d7af6cd9ced280ec2097
Theo Rocha <theomarquesrx@gmail.com>
2022-07-07 23:33:40 -0300

    Retorna o resultado esperado(0) quando o jogo -dá velha-, ou seja, termina empatado

--- testa_velha.py
+++ testa_velha.py
@@ -1,13 +1,17 @@
 import velha
 
+# No começo do teste de cada caso, os outros testes foram comentados para uma melhor leitura do problema propostp. 
+
 l1 = [2,1,2]
 l2 = [1,2,1]
-l3 = [1,1,2]
+l3 = [1,2,1]
 
 # def test_Xganhou():
  #    assert 1 == velha.velha(l1,l2,l3)
 
-def test_Oganhou(): #unico caso possível em que o O ganha é na diagonal.
-    assert 2 == velha.velha(l1,l2,l3)
+# def test_Oganhou(): #unico caso possível em que o O ganha é na diagonal.
+  #  assert 2 == velha.velha(l1,l2,l3)
 
+def test_JogoEmpatado(): 
+    assert 0 == velha.velha(l1,l2,l3)
 

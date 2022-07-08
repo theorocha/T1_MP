1c0d07ead9c3d4533a1430811e8a040a08629208
Theo Rocha <theomarquesrx@gmail.com>
2022-07-07 23:38:52 -0300

    Testa o 1º caso do jogo impossível: dois ganhadores

--- testa_velha.py
+++ testa_velha.py
@@ -2,8 +2,8 @@ import velha
 
 # No começo do teste de cada caso, os outros testes foram comentados para uma melhor leitura do problema propostp. 
 
-l1 = [2,1,2]
-l2 = [1,2,1]
+l1 = [1,1,1]
+l2 = [2,2,2]
 l3 = [1,2,1]
 
 # def test_Xganhou():
@@ -12,6 +12,8 @@ l3 = [1,2,1]
 # def test_Oganhou(): #unico caso possível em que o O ganha é na diagonal.
   #  assert 2 == velha.velha(l1,l2,l3)
 
-def test_JogoEmpatado(): 
-    assert 0 == velha.velha(l1,l2,l3)
+# def test_JogoEmpatado(): 
+   #assert 0 == velha.velha(l1,l2,l3)
 
+def test_JogoImpossivel():
+    assert -2 == velha.velha(l1,l2,l3)

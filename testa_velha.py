a7d90211bbdfb4ca60bb382e15ea8841cd1ead03
Theo Rocha <theomarquesrx@gmail.com>
2022-07-07 23:28:46 -0300

    Único caso possível em que o O é vencedor: combinação na diagonal...Testes anteriores foram comentados para uma melhor compreensão

--- testa_velha.py
+++ testa_velha.py
@@ -1,10 +1,13 @@
 import velha
 
-l1 = [1,2,2]
-l2 = [1,1,2]
-l3 = [1,2,1]
+l1 = [2,1,2]
+l2 = [1,2,1]
+l3 = [1,1,2]
 
-def test_Xganhou():
-     assert 1 == velha.velha(l1,l2,l3)
+# def test_Xganhou():
+ #    assert 1 == velha.velha(l1,l2,l3)
+
+def test_Oganhou(): #unico caso possível em que o O ganha é na diagonal.
+    assert 2 == velha.velha(l1,l2,l3)
 
 

92b4e730b7a550b273c1a063eb9281c5ea654a88
Theo Rocha <theomarquesrx@gmail.com>
2022-07-07 23:51:40 -0300

    Apenas o jogo impossivel testa com sucesso.

--- testa_velha.py
+++ testa_velha.py
@@ -2,8 +2,8 @@ import velha
 
 # No começo do teste de cada caso, os outros testes foram comentados para uma melhor leitura do problema propostp. 
 
-l1 = [2,1,1]
-l2 = [1,1,2]
+l1 = [2,2,2]
+l2 = [1,2,2]
 l3 = [2,2,1]
 
 def test_Xganhou():
